f5c9207ecf6790d3b1408e317513dbbe11dc4bb6
sudarshan <sudarshan2402@gmail.com>
2024-01-20 15:30:26 +0800

    Add hand point tracking with drawing

--- signlang.py
+++ signlang.py
@@ -1,39 +1,42 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
 
-cap = cv2.VideoCapture(0)
-## Setup mediapipe instance
-with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
 
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+def sign_language():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
 
-        # Make detection
-        results = hands.process(image)
+    cap = cv2.VideoCapture(0)
+    # Setup mediapipe instance
+    with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            ret, frame = cap.read()
 
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
 
-        # Check if multiple hands
-        if results.multi_hand_landmarks:
-            # Render detections
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(image, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                      )
+            # Make detection
+            results = hands.process(image)
 
-        cv2.imshow('Mediapipe Feed', image)
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+            # Check if multiple hands
+            if results.multi_hand_landmarks:
+                # Render detections
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(image, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                          mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                          )
 
-    cap.release()
-    cv2.destroyAllWindows()
+            cv2.imshow('Mediapipe Feed', image)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
